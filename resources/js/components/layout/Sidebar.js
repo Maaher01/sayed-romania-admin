101aafb5e75e272bec3ea59c87bf4f58c5b00fa4
Maaher01 <maaher1145@gmail.com>
2024-03-31 16:29:00 +0600

    Sunday commit

--- resources/js/components/layout/Sidebar.js
+++ resources/js/components/layout/Sidebar.js
@@ -31,85 +31,85 @@ import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
 const drawerWidth = 230;
 
 const ClippedDrawer = () => {
-	const [name, setName] = useState("");
-	const [imageUrl, setImageUrl] = useState(null);
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
 
-	useEffect(() => {
-		fetchProfile();
-	}, []);
+  useEffect(() => {
+    fetchProfile();
+  }, []);
 
-	const fetchProfile = async () => {
-		await axios
-			.get(`/api/companysetup`)
-			.then(({ data }) => {
-				const alldata = data.data[0];
-				setName(alldata._name);
-				setImageUrl(alldata._image);
-			})
-			.catch(({ response: { data } }) => {
-				toast("No Data Found");
-			});
-	};
+  const fetchProfile = async () => {
+    await axios
+      .get(`/api/companysetup`)
+      .then(({ data }) => {
+        const alldata = data.data[0];
+        setName(alldata._name);
+        setImageUrl(alldata._image);
+      })
+      .catch(({ response: { data } }) => {
+        toast("No Data Found");
+      });
+  };
 
-	return (
-		<Box sx={{ display: "flex" }}>
-			<Drawer
-				variant="permanent"
-				sx={{
-					width: drawerWidth,
-					flexShrink: 0,
-					[`& .MuiDrawer-paper`]: {
-						width: drawerWidth,
-						boxSizing: "border-box",
-					},
-				}}
-			>
-				<Toolbar
-					style={{ minHeight: "69px", borderBottom: "1px solid #d8dbe0" }}
-				>
-					<Link style={{ textDecoration: "none" }} to="/app/dashboard">
-						<img
-							src={imageUrl}
-							alt="nothing"
-							style={{ marginTop: "2px", width: "150px", height: "70px" }}
-						/>
-					</Link>
-				</Toolbar>
-				<Box sx={{ overflow: "auto" }}>
-					<List>
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/dashboard/companysetup"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<SettingsIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Company Setup"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/country"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<FlagIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Country"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
-						{/* <Link
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar
+          style={{ minHeight: "69px", borderBottom: "1px solid #d8dbe0" }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/app/dashboard">
+            <img
+              src={imageUrl}
+              alt="nothing"
+              style={{ marginTop: "2px", width: "150px", height: "70px" }}
+            />
+          </Link>
+        </Toolbar>
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/dashboard/companysetup"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <SettingsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Company Setup"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/country"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FlagIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Country"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
               style={{
                 textDecoration: "none",
                 color: "rgba(44, 56, 74, .681)",
@@ -124,8 +124,8 @@ const ClippedDrawer = () => {
                   <ListItemText primary={"Degree"} />
                 </ListItemButton>
               </ListItem>
-            </Link> */}
-						{/* <Link
+            </Link>
+            <Link
               style={{
                 textDecoration: "none",
                 color: "rgba(44, 56, 74, .681)",
@@ -140,262 +140,262 @@ const ClippedDrawer = () => {
                   <ListItemText primary={"University"} />
                 </ListItemButton>
               </ListItem>
-            </Link> */}
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/course"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<MenuBookIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Course"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
-					</List>
-					<Divider />
-					<List>
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/client"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<SupervisedUserCircleIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Client"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/contact"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<PermContactCalendarIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Contact"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/image"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<CameraAltIcon />
-									</ListItemIcon>
-									<ListItemText primary={"University Image"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/slider"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<TuneIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Slider"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/menu"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<WidgetsIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Menu"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/section"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<AddRoadIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Section"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/course"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <MenuBookIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Course"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          </List>
+          <Divider />
+          <List>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/client"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <SupervisedUserCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Client"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/contact"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <PermContactCalendarIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Contact"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/image"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <CameraAltIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"University Image"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/slider"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <TuneIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Slider"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/menu"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <WidgetsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Menu"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/section"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AddRoadIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Section"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
 
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/review"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<RemoveRedEyeIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Reviews"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/faq"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<HelpOutlineIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Question"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/review"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <RemoveRedEyeIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Reviews"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/faq"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <HelpOutlineIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Question"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
 
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/newsfeed"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<FeedIcon />
-									</ListItemIcon>
-									<ListItemText primary={"News Feed"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/newsfeed"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <FeedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"News Feed"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
 
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/counter"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<AddCircleIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Counters"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/counter"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AddCircleIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Counters"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
 
-						<Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/recentvisasuccess"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<WorkHistoryIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Recent Visas"} />
-								</ListItemButton>
-							</ListItem>
-						</Link>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/recentvisasuccess"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <WorkHistoryIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Recent Visas"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
 
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/sociallink"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<ShareIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Social Link"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/sociallink"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <ShareIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Social Link"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
 
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/event"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<EventIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Event"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/event"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <EventIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Event"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
 
-						{/* <Link
-							style={{
-								textDecoration: "none",
-								color: "rgba(44, 56, 74, .681)",
-							}}
-							to="/app/studentregistration"
-						>
-							<ListItem disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										<AppRegistrationIcon />
-									</ListItemIcon>
-									<ListItemText primary={"Student Reg"} />
-								</ListItemButton>
-							</ListItem>
-						</Link> */}
-					</List>
-				</Box>
-			</Drawer>
-		</Box>
-	);
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "rgba(44, 56, 74, .681)",
+              }}
+              to="/app/studentregistration"
+            >
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <AppRegistrationIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"Student Reg"} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  );
 };
 
 export default ClippedDrawer;
